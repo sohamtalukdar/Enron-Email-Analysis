85584197b62f45ed280a43cfc1fdba14c79d395a
sohamtalukdar <103217802+sohamtalukdar@users.noreply.github.com>
2023-02-04 06:38:38 +0530

    updates

---
+++ libraries.py
@@ -0,0 +1,25 @@
+import os
+import csv
+import numpy as np
+import pandas as pd
+import nltk
+nltk.download('punkt')
+nltk.download('stopwords')
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+import nltk.stem as stemmer
+from sklearn.feature_extraction.text import TfidfVectorizer
+import string
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+import re
+from nltk.stem import SnowballStemmer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import accuracy_score, classification_report
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.svm import SVC
+import gensim
+from gensim.models import Word2Vec
