5c7e386e49c118b84f1ccb4a36f1387b07262851
sohamtalukdar <103217802+sohamtalukdar@users.noreply.github.com>
2023-02-05 09:48:41 +0530

    Update libraries.py

--- libraries.py
+++ libraries.py
@@ -23,8 +23,6 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.svm import SVC
 import gensim
 from gensim.models import Word2Vec
-import numpy as np
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
-import numpy as np
