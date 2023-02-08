0f89b34902c5fb9ef93b36fdf285019fca055c33
sohamtalukdar <103217802+sohamtalukdar@users.noreply.github.com>
2023-02-08 22:21:14 +0530

    Update MLP.py

--- MLP.py
+++ MLP.py
@@ -12,6 +12,46 @@ tc = TextClassifier(preprocessor)
 tc.split_data()
 
 
+class TextClassifier:
+    def __init__(self, preprocessor, sg=0, window=5, min_count=1, negative=10, seed=0):
+        self.preprocessor = preprocessor
+        self.df = preprocessor.get_dataframe()
+        self.messages = self.df['Message'].apply(lambda x: x.split()).tolist()
+        self.model = Word2Vec(self.messages, sg=sg, window=window, min_count=min_count, negative=negative, seed=seed)
+
+    def split_data(self, test_size=0.2, random_state=0):
+        self.train_df, self.test_df = train_test_split(self.df, test_size=test_size, random_state=random_state)
+        self.train_labels = self.train_df['Label'].tolist()
+        self.train_messages = self.train_df['Message'].apply(lambda x: x.split()).tolist()
+        self.test_labels = self.test_df['Label'].tolist()
+        self.test_messages = self.test_df['Message'].apply(lambda x: x.split()).tolist()
+
+    def get_average_word_vectors(self, messages):
+        vectors = np.zeros((len(messages), 100))
+        for i, message in enumerate(messages):
+            word_vectors = np.zeros((100,))
+            for word in message:
+                if word in self.model.wv:
+                    word_vectors += self.model.wv[word]
+            word_vectors = word_vectors / len(message)
+            vectors[i] = word_vectors
+        return vectors
+
+    def grid_search(self, gridsearch):
+        train_vectors = self.get_average_word_vectors(self.train_messages)
+        gridsearch.fit(train_vectors, self.train_labels)
+        return
+
+    
+    def train_and_evaluate(self, classifier):
+        train_vectors = self.get_average_word_vectors(self.train_messages)
+        classifier.fit(train_vectors, self.train_labels)
+
+        test_vectors = self.get_average_word_vectors(self.test_messages)
+        predictions = classifier.predict(test_vectors)
+
+ 
+
 # Define the hyperparameter search space
 param_grid = {'hidden_layer_sizes': [(50,), (100,), (150,)],
               'activation': ['identity', 'logistic', 'tanh', 'relu'],
